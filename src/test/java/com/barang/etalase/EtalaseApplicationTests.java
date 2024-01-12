b6bbc53f0c591d9df03260460c4a23ef0f4d3402
davidsimanjuntak3 <53857852+davidsimanjuntak3@users.noreply.github.com>
2024-01-12 22:09:53 +0700

    Add files via upload

---
+++ src/test/java/com/barang/etalase/EtalaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barang.etalase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtalaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
